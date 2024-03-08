e169d13d9ef973e8d845bc1a1b4c8cbe308a716b
matthewvdw <matthew.vanderw@gmail.com>
2024-03-08 14:29:12 +0200

    changed remove to an update status

--- src/lib/db/schema/todos.db.ts
+++ src/lib/db/schema/todos.db.ts
@@ -1,11 +1,13 @@
 import { sql } from "drizzle-orm";
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 
+export type Status = 'active' | 'completed' | 'deleted';
+
 export const todos = sqliteTable('todos', {
     id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
     title: text('title').notNull(),
     description: text('description').notNull(),
-    status: text('status').default('active').notNull().$type<'active' | 'completed' | 'deleted'>(),
+    status: text('status').default('active').notNull().$type<Status>(),
     completedAt: integer('completed_at', { mode: 'timestamp_ms' }),
     createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
 });
---
+++ src/lib/helpers/status.todo.ts
@@ -0,0 +1,37 @@
+/* 
+
+This function will be used to updated a todos status. 
+It will take in a status and return the updated todo.
+
+*/
+
+
+import { db } from "$lib/db/client.db";
+import { todos, type Status } from "$lib/db/schema/todos.db";
+import { eq } from "drizzle-orm";
+
+
+export async function setStatus(id: number, status: Status) {
+    try {
+        const [todo] = await db.update(todos)
+            .set({
+                status,
+                completedAt: status === 'completed' ? new Date() : null
+            })
+            .where(eq(todos.id, id)).returning()
+
+        return {
+            resultStatus: "success",
+            resultMessage: "Todo status updated successfully",
+            data: todo
+        }
+
+    } catch (error) {
+        console.error(error)
+
+        return {
+            resultStatus: "error",
+            resultMessage: "An error occurred while updating todos status",
+        }
+    }
+}
