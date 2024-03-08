53a4dc95c16ecab00ef4270e072db9b6a728571c
Inf3ern0 <93455596+Inf3ern0@users.noreply.github.com>
2024-03-08 12:34:24 +0200

    Some error handling done

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,28 +1,37 @@
-<script>
-    let todos = [];
-    let newTodo = '';
+<script lang="ts">
+    interface Todo {
+      id: number;
+      text: string;
+      done: boolean;
+    }
   
+    let todos: Todo[] = [];
+    let newTodo: string = '';
+  
+    //Adding a task on my todo list
     function addTodo() {
-      if (newTodo.trim() !== '') {
-        todos = [...todos, { id: Date.now(), text: newTodo, done: false }];
-        newTodo = '';
-      }
+    if (newTodo.trim() !== '') {
+      todos = [...todos, { id: Date.now(), text: newTodo, done: false }];
+      newTodo = '';
     }
-  
-    function toggleDone(todo) {
-      todo.done = !todo.done;
-      todos = todos.map(t => t.id === todo.id ? todo : t);
-    }
-  
-    function removeTodo(todoId) {
-      todos = todos.filter(t => t.id !== todoId);
-    }
-  </script>
+  }
+// Marking task as done
+  function toggleDone(todo: Todo) {
+    todo.done = !todo.done;
+    todos = todos.map(t => t.id === todo.id ? todo : t);
+  }
+//Remove task from list
+  function removeTodo(todoId: number) {
+    todos = todos.filter(t => t.id !== todoId);
+  }
+</script>
   
   <style>
+    
     .done {
       text-decoration: line-through;
     }
+    
   </style>
   
   <h1>Todo List</h1>
