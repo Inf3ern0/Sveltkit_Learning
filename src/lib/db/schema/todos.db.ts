7a948b65585e2d26e53f80deff6aa63fb567720a
matthewvdw <matthew.vanderw@gmail.com>
2024-03-08 13:46:09 +0200

    added not null to the schema

--- src/lib/db/schema/todos.db.ts
+++ src/lib/db/schema/todos.db.ts
@@ -3,8 +3,8 @@ import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 
 export const todos = sqliteTable('todos', {
     id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
-    title: text('title'),
-    description: text('description'),
+    title: text('title').notNull(),
+    description: text('description').notNull(),
     completedAt: integer('completed_at', { mode: 'timestamp_ms' }),
-    createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`CURRENT_TIMESTAMP`),
+    createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
 });
