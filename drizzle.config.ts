ac576177d38edf8ccebe35ee2cef77ab1c2e7765
matthewvdw <matthew.vanderw@gmail.com>
2024-03-08 13:29:41 +0200

    updated schema directory

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    schema: "./schema/*",
+    schema: "./src/lib/db/schema/*",
     out: "./drizzle",
     driver: 'turso',
     dbCredentials: {
