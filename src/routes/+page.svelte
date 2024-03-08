9c3114561a3a5e634bdc20b3ba1e971c1a26ca12
Inf3ern0 <93455596+Inf3ern0@users.noreply.github.com>
2024-03-08 12:52:58 +0200

    fixed errors and got to show on open page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    interface Todo {
+      id: number;
+      text: string;
+      done: boolean;
+    }
+  
+    let todos: Todo[] = [];
+    let newTodo: string = '';
+  
+    //Adding a task on my todo list
+    function addTodo() {
+    if (newTodo.trim() !== '') {
+      todos = [...todos, { id: Date.now(), text: newTodo, done: false }];
+      newTodo = '';
+    }
+  }
+// Marking task as done
+  function toggleDone(todo: Todo) {
+    todo.done = !todo.done;
+    todos = todos.map(t => t.id === todo.id ? todo : t);
+  }
+//Remove task from list
+  function removeTodo(todoId: number) {
+    todos = todos.filter(t => t.id !== todoId);
+  }
+</script>
+
+<style>
+    
+    .done {
+      text-decoration: line-through;
+    }
+    
+  </style>
+  
+  <h1>Todo List</h1>
+  <input bind:value={newTodo} on:keyup={event => event.key === 'Enter' && addTodo()} placeholder="Add new todo">
+  <button on:click={addTodo}>Add</button>
+  
+  <ul>
+    {#each todos as todo}
+      <li class:done={todo.done}>
+        <input type="checkbox" bind:checked={todo.done} on:click={() => toggleDone(todo)}>
+        {todo.text}
+        <button on:click={() => removeTodo(todo.id)}>Remove</button>
+      </li>
+    {/each}
+  </ul>
