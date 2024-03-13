eeed97025a2d2ae9f475dc36a2cfc68226d8123a
Inf3ern0 <93455596+Inf3ern0@users.noreply.github.com>
2024-03-13 14:15:34 +0200

    SQL Table creation

--- src/lib/db/client.db.ts
+++ src/lib/db/client.db.ts
@@ -6,3 +6,4 @@ import { PRIVATE_DB_TOKEN, PRIVATE_DB_URL } from '$env/static/private';
 const client = createClient({ url: PRIVATE_DB_URL, authToken: PRIVATE_DB_TOKEN });
 
 export const db = drizzle(client);
+
--- src/lib/db/todos.db.ts
+++ src/lib/db/todos.db.ts
@@ -1,10 +1,9 @@
-import { sql } from "drizzle-orm";
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
+
+export type Status = 'active' | 'completed' | 'deleted';
 
 export const todos = sqliteTable('todos', {
     id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
-    title: text('title'),
-    description: text('description'),
-    completedAt: integer('completed_at', { mode: 'timestamp_ms' }),
-    createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`CURRENT_TIMESTAMP`),
-});
+    description: text('description').notNull(),
+    status: text('status').default('active').notNull().$type<Status>(),
+})
